70c3c599043b0495fc76b15b5ff509bf488d6d42
hoquynhhh <hodangquynh@dtu.edu.vn>
2026-02-04 09:00:35 +0700

    chuyển đổi các biến môi trường sang .env

--- backend/src/main/java/com/skillbridge/backend/BackendApplication.java
+++ backend/src/main/java/com/skillbridge/backend/BackendApplication.java
@@ -2,11 +2,20 @@ package com.skillbridge.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")   // thư mục backend
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(BackendApplication.class, args);
     }
 
