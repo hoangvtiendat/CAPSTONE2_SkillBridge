fa0424bafc60971adc21544a87a75a87509a686e
Mai Quoc Toan <quctonnn@gmail.com>
2026-02-06 15:59:08 +0700

    sprint1/JD_Detail

--- frontend/src/components/company/JD_of_company.jsx
+++ frontend/src/components/company/JD_of_company.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-
+import "./JD_of_companny.css";
 
 const JD_of_company = () => {
       const {id} = useParams();
@@ -41,11 +41,72 @@ const JD_of_company = () => {
                 <div>Loading...</div>
             ) : job ? (
                 <div>
-                    <h1>{job.position}</h1>
-                    <p>Company: {job.company}</p>
-                    <p>Location: {job.location}</p>
-                    <p>Salary: ${job.salary?.min}k - ${job.salary?.max}k</p>
-                    <p>Description: {job.description}</p>
+                    <div className="title_header">
+                        <div className="in4_company">
+                            <div className="img_logo">
+                                <img src={job.logo} alt="Company Logo" />
+                            </div>
+                            <div className="detail_company">
+                                <h1 className="postion-tag">
+                                    <i className="fas fa-user-tie"></i> {job.position}
+                                </h1>
+                                <h1 className="company-tag">
+                                    <i className="fas fa-building"></i> {job.company}
+                                </h1>
+                                <div className="location-status">
+                                    <h1 className="location-tag">
+                                        <i className="fas fa-map-marker-alt"></i> {job.location}
+                                    </h1>
+                                    <div className="dot"></div>
+                                    <h1 className="status-tag">
+                                        <i className="fas fa-briefcase"></i> {job.status}
+                                    </h1>
+                                </div>
+                            </div> 
+                        </div>
+                        <div className="tags_of_JD">
+                             {/* tag short_description */}
+                                <div className="short_description">
+                                    <div className="tag-description">
+                                        {job.tags.map((tag, index) => 
+                                            <span key={index} className="tag-item">{tag}</span>
+                                        )}
+                                    </div>
+
+                                </div>
+                        </div>
+                        <div className="Action_tag">
+                            <button className="apply_now">Ứng tuyển ngay </button>
+                            <button className="AI_analyze">AI phân tích</button>
+                        </div>
+                    </div>
+                    <div className="description">
+                        { job.in4 && job.in4.map((section, index) => {
+                            const key = Object.keys(section)[0];
+                            const sectionData = section[key];
+                            return (
+                                <div className="jd-section" key={index}>
+                                    <h2 className="jd-title">{sectionData.title}</h2>
+
+                                    {/* description is a string */}
+                                    {typeof sectionData.description === "string" && (
+                                        <p className="jd-text">{sectionData.description}</p>
+                                    )}
+
+                                    {/* description is an array */}
+                                    {Array.isArray(sectionData.description) && (
+                                        <ul className="jd-list">
+                                            {sectionData.description.map((line, i) => (
+                                                <li key={i}>{line}</li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </div>
+                            );
+                        } )}
+                    </div>
+                
+
                 </div>
             ) : (
                 <div>No job found.</div>
--- frontend/src/pages/company/JD_of_campany.jsx
+++ frontend/src/pages/company/JD_of_campany.jsx
@@ -3,19 +3,26 @@ import Header from '../../components/home/Header';
 import Sidebar from '../../components/home/Sidebar';
 import Hero from '../../components/home/Hero';
 import JD_of_company from '../../components/company/JD_of_company';
+import Stats from '../../components/home/Stats';
+import CTA from '../../components/home/CTA';
+import Footer from '../../components/home/Footer';
 const JD_of_companyPage = () => {
-    return(
-    <div className="home-page">
-        <Header />
-        <div className="home-container">
-        <main className="JD_of_company-main">
-            <JD_of_company />
-        </main>
-        </div>
-    </div>
 
-       
-    );
+
+      return (
+    <div className="home-page">
+      <Header />
+      
+      <div className="home-container">
+        <Sidebar />
+        
+        <main className="JD_of_company-main">
+           <JD_of_company />
+        </main>
+      </div>
+   
+    </div>
+  );
 }
 
 
