80d682717daa431842bf7dbd30516ef9f3402f97
HOANG VAN TIEN DAT <140961999+hoangvtiendat@users.noreply.github.com>
2026-02-13 15:29:09 +0700

    add useEffect in Authcontext

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import userService from '../services/api/userService';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
